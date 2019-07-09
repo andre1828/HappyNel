fc5a9eaabe867e5e6c0d302c1073b4fc3492529a
andre1828 <andremelo13@gmail.com>
2019-07-08 22:30:08 -0300

    Create routes for initial page and member listing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'pagina_inicial#pagina_inicial'
+  get '/membros', to: 'membros#listar_membros', as: 'listar_membros'
 end
